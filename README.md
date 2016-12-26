1de6ac050f841d2469618d4f67ab1e7dabdc03b3
Jhonbeltran <jbeltranleon@gmail.com>
2016-12-26 17:00:28 -0500

    Agrego git ignore

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-ómo organizar los archivos en un proyecto?
+Cómo organizar los archivos en un proyecto?
 File-Type First (FTF), donde colocamos todos los archivos de un mismo tipo en una carpeta.
 Feature First (Pods), donde colocamos cada feature en una carpeta.
 Apps of Apps, creamos una carpeta por cada aplicación
---
+++ nodeproject.js/source/server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import http from 'http'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+
+function requestHandler(request, response) {
+  const html = renderToString(
+    React.DOM.h1('hache uno con texto')
+  )
+
+  response.write(html)
+  response.end()
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(3000)
